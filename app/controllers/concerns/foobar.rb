f55b1dc8ff9beaca06a24f8f14267e6cd25f156f
Suguna Simhadri <simhadrisugun716@students.itu.edu>
2017-04-24 10:19:12 +0000

    Q2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,14 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  
+    def initialize(b)
+     hash = Hash.new(b)
+    end
+    def bar(arg, args = {})
+        puts arg
+        args.each{|v|
+            v            
+        }
+        
+    end
 end
