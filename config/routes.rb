ca5ed0edee2ca251b98b3ac86a86f36c57a9bbb3
Suguna Simhadri <simhadrisugun716@students.itu.edu>
2017-04-24 09:56:40 +0000

    Q1.1

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'age', to: 'pages#age'
+  get 'stringify', to:'pages#stringify'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
